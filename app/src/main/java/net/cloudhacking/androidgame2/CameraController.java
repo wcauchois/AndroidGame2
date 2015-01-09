fc2a3d372d5aaa832040bbdc4530cfd4b0c2adba
Bill Cauchois <wcauchois@foursquare.com>
2015-01-09 02:33:37 +0000

    remove debug statement

--- app/src/main/java/net/cloudhacking/androidgame2/CameraController.java
+++ app/src/main/java/net/cloudhacking/androidgame2/CameraController.java
@@ -1,7 +1,5 @@
 package net.cloudhacking.androidgame2;
 
-import android.util.Log;
-
 /**
  * Created by wcauchois on 1/8/15.
  */
@@ -20,7 +18,6 @@ public class CameraController {
         }
 
         public void onDrag(Vector2 currentPos, Vector2 posDelta) {
-            Log.d("FOO", "got drag delta " + posDelta);
             // TODO(wcauchois): If we implement zooming this will have to take that into account.
             Vector2 newCameraPos = mStartDragCameraPos.add(posDelta.negate());
             mCamera.setPosition(newCameraPos);
